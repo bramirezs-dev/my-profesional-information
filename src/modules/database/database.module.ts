50195bdea8fab2a69c7a37e647b218f7116493de
brian.ramirez <brian.ramirez@overactive.com>
2023-02-09 10:30:25 -0500

    add conection mongo remote

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -11,13 +11,16 @@ import {MongooseModule} from '@nestjs/mongoose'
         MongooseModule.forRootAsync({
             useFactory: async (configService:ConfigService)=>{
                 
-                if (configService.get<string>('MONGO_HOST')){
-                    return {
+                if (configService.get<string>('NODE_ENV') === "pro"){
+                    
+                    const connect = {
                         uri:`mongodb://${(configService.get<string>('MONGO_HOST'))}:${(configService.get<string>('MONGO_PORT'))}`,
                         user:configService.get<string>('MONGO_USER'),
                         pass:configService.get<string>('MONGO_PASS'),
                         dbName: configService.get<string>('MONGO_DB')
                     }
+                    
+                    return connect;
                 }
                 
                 return {
@@ -35,6 +38,15 @@ import {MongooseModule} from '@nestjs/mongoose'
     providers:[{
         provide:'MONGO',
         useFactory: async (configService:ConfigService) => {
+            
+            if(configService.get<string>('NODE_ENV') === 'pro'){
+                const uri = `mongodb://${configService.get<string>('MONGO_USER')}:${configService.get<string>('MONGO_PASS')}@${configService.get<string>('MONGO_HOST')}:${configService.get<string>('MONGO_PORT')}`;
+                const client = new MongoClient(uri);
+                await client.connect();
+                const database = client.db(configService.get<string>('MONGO_DB')  || configService.get<string>('mongodbConfig.database') );
+                return database;
+            }
+            
             const uri = `mongodb://${(configService.get<string>('MONGO_HOST') || configService.get<string>('mongodbConfig.host')  )}/?readPreference=primary&ssl=false&directConnection=true`;
             const client = new MongoClient(uri);
             await client.connect();
